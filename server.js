0b0518966186c2dcf9374ca141f8821ef89dbb6c
wjk36 <57546377+wjk36@users.noreply.github.com>
2019-12-08 14:44:08 -0500

    Merged Chris's Changes

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var cors = require("cors");
 const fs      = require("fs");
 const jwt     = require("jsonwebtoken");
 var spotify_data = require( "./spotify.json" );
-
+var mysql = require('mysql');
+var ID ='test';
+var profileLink = 'testlink'
+var profilePic = 'testpic'
+var key = require('./dbkey.json');
 
 var app = express();
 
@@ -65,7 +69,7 @@ app.get("/grabPlaylists", function (req, res) {
         request.get({url: "https://api.spotify.com/v1/users/" + data.id + "/playlists", headers: {"Authorization": "Bearer " + access_token}}, function (error, response, body2) {
             var data2 = JSON.parse(body2);
             for (i=0; i<data2.items.length; i++) {
-                html += '<div class="checkboxes"><input type="checkbox" class="checkbox" value="' + data2.items[i].tracks.href + '"/>  ' + data2.items[i].name + '</div>';
+                html += '<div class="checkboxes"><input onclick="transferSelected()" type="checkbox" class="checkbox" value="' + data2.items[i].tracks.href + '"/>  ' + data2.items[i].name + '</div>';
             }
             res.send(html);
             res.end();
@@ -87,7 +91,6 @@ app.get("/makePlay", function(req,res) {
             "Music-User-Token": userKey
         }
     }, function (error, response, body) {
-            console.log(body);
             res.send(response);
     });
 });
@@ -115,3 +118,89 @@ app.post("/transferSelected", function(req,res) {
             });
     }
 });
+
+var con = mysql.createConnection({
+    host: 'swapify.mysql.database.azure.com',
+    user: key.user,
+    password: key.password,
+    database: 'swapify',
+    port: 3306,
+    ssl: true
+    });
+
+con.connect(
+    function(err){
+        if(err){
+            console.log("Error Connecting to Swapify DB.");
+        }
+        else{
+            console.log("Successful connection to Swapify DB.")
+        }
+    });
+
+//TO DO: Save user info/init user (ID, Profile Link, #Swaps)
+
+exports.updateUser = function (ID, profileLink, profilePic){
+    con.query(`INSERT IGNORE INTO Curators (curatorID, profileLink, profilePic, swapCount) VALUES ("${ID}", "${profileLink}", "${profilePic}", 0);`,
+               function(err, results, fields){
+                    if(err) throw err;
+                    else console.log('Updated ' + results.affectedRows + 'row(s).');
+    })
+
+    con.query(`UPDATE Curators SET swapCount = swapCount + 1 WHERE curatorID = "${ID}";`,
+               function(err, results, fields){
+                    if(err) throw err;
+                    else console.log('Updated ' + results.affectedRows + 'row(s).');
+    })
+
+};
+
+//TO DO: Save swapped playlist info (Playlist Name, Curator, Transfer Count)
+
+exports.updatePlaylists = function (title, curator){
+    con.query(`INSERT IGNORE INTO Playlists (title, curatorID, transferCount) VALUES ("${title}", "${curator}", 0);`,
+               function(err, results, fields){
+                    if(err) throw err;
+                    else console.log('Updated ' + results.affectedRows + 'row(s).');
+    })
+
+    con.query(`UPDATE Playlists SET transferCount = transferCount + 1 WHERE title = "${title}";`,
+               function(err, results, fields){
+                    if(err) throw err;
+                    else console.log('Updated ' + results.affectedRows + 'row(s).');
+    })
+};
+
+//TO DO: Pull top 5 transferred playlists
+
+exports.showTopPlaylists = function(){
+    con.query(`SELECT title FROM Playlists ORDER BY DESC LIMIT 0,5;`,
+             function(err, results, fields){
+                    if(err) throw err;
+                    else console.log('Returning Popular Playlists...' + results);
+        return results;
+    })
+};
+
+//TO DO: Pull top 5 frequent users
+
+exports.showFrequentUsers = function(){
+        con.query(`SELECT curatorID FROM Curators ORDER BY DESC LIMIT 0,5;`,
+             function(err, results, fields){
+                    if(err) throw err;
+                    else console.log('Returning frequent users...' + results);
+            return results;
+    })
+};
+
+//TO DO: Pull a random playlist
+
+exports.feelingLucky = function() {
+   idx = Math.floor((Math.random() * 2) + 1);
+            con.query(`SELECT title FROM Playlists ORDER BY DESC LIMIT ${idx},1;`,
+             function(err, results, fields){
+                    if(err) throw err;
+                    else console.log('Feeling Lucky?: ' + results);
+            return results;
+    })
+}
